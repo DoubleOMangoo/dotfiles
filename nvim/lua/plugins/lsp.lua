ef624557e54f0ac709e20032e6c6d32aae9fdc57
DoubleOMangoo <mhafizidlan@gmail.com>
2025-03-03 01:43:50 +0800

    Modified helix colorscheme, updated nvim plugins & added rust-analyzer to nvim

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -28,6 +28,7 @@ return
 			local capabilities = require("blink.cmp").get_lsp_capabilities()
 			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup({ capabilities = capabilities })
+			lspconfig.rust_analyzer.setup({ capabilities = capabilities })
 		end,
 	}
 
