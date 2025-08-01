49bf8b197f59eccb59d455796e5dd9f0a3233b41
DoubleOMangoo <mhafizidlan@gmail.com>
2025-08-01 16:16:59 +0800

    Add leader key indicator for wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -63,5 +63,29 @@ config.key_tables = {
 		{ key = "Enter", action = "PopKeyTable" },
 	},
 }
+
+-- Leader key indicator
+wezterm.on("update-right-status", function(window, _)
+		local SOLID_LEFT_ARROW = ""
+		local ARROW_FOREGROUND = {Foreground = {Color = "#c6a0f6"}}
+		local prefix = ""
+
+		if window:leader_is_active() then
+			prefix = " " .. utf8.char(0x1f30a)
+			SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+		end
+
+		if window:active_tab():tab_id() ~0 then
+			ARROW_FOREGROUND = {Foreground = {Color = "#1e2030"}}
+		end
+
+		window:set_left_status(wezterm.format {
+			{Background = {Color = "#b7bdf8"}},
+			{Text = prefix},
+			ARROW_FOREGROUND,
+			{Text = SOLID_LEFT_ARROW}
+		})
+	end
+)
 -- and finally, return the configuration to wezterm
 return config
