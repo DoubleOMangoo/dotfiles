98e6d702698f003429aaaa84bdff38f79e5e2016
DoubleOMangoo <mhafizidlan@gmail.com>
2025-08-01 14:42:17 +0800

    Add keybinds for manipulating panes and tabs in wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,22 +1,67 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
+local act = wezterm.action
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
-
-config.color_scheme = 'Mangooscheme'
+-- Settings
+config.color_scheme = "Mangooscheme"
 config.color_schemes = {
-  ['Mangooscheme'] = {
-    background = "#1c1431"
-  }
+	["Mangooscheme"] = {
+		background = "#1c1431",
+	},
 }
 config.max_fps = 120
-config.default_prog = { "powershell.exe"}
-config.tab_bar_at_bottom = true;
---config.color_scheme = "Tokyo Night Moon"
+config.default_prog = { "powershell.exe" }
+config.tab_bar_at_bottom = true
+
+-- Keys
+config.leader = { key = "Space", mods = "CTRL", timeout_milliseconds = 1000 }
+config.keys = {
+	{ key = "c", mods = "LEADER", action = act.SendKey({ key = "a", mods = "CTRL" }) },
+	{ key = "c", mods = "LEADER", action = act.ActivateCopyMode },
+
+	-- Pane keybindings
+
+	{ key = "-", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "\\", mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
+	{ key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
+	{ key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
+	{ key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
+	{ key = "x", mods = "LEADER", action = act.CloseCurrentPane({ confirm = true }) },
+	{ key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
+
+	{ key = "r", mods = "LEADER", action = act.ActivateKeyTable({ name = "resize_pane", one_shot = false }) },
+
+	--Tab keybindings
+	{ key = "n", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
+	{ key = "[", mods = "LEADER", action = act.ActivateTabRelative(-1) },
+	{ key = "]", mods = "LEADER", action = act.ActivateTabRelative(1) },
+	{ key = "t", mods = "LEADER", action = act.ShowTabNavigator },
+
+	-- Workspace
+	{ key = "w", mods = "LEADER", action = act.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }) },
+}
+
+for i = 1, 9 do
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "LEADER",
+		action = act.ActivateTab(i - 1),
+	})
+end
+
+config.key_tables = {
+	resize_pane = {
+		{ key = "h", action = act.AdjustPaneSize({ "Left", 1 }) },
+		{ key = "j", action = act.AdjustPaneSize({ "Down", 1 }) },
+		{ key = "k", action = act.AdjustPaneSize({ "Up", 1 }) },
+		{ key = "l", action = act.AdjustPaneSize({ "Right", 1 }) },
+		{ key = "Escape", action = "PopKeyTable" },
+		{ key = "Enter", action = "PopKeyTable" },
+	},
+}
 -- and finally, return the configuration to wezterm
 return config
